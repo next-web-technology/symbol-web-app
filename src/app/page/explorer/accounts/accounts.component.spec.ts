8d2e28f768e904679fc950af73462d433a1172b2
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-09-19 16:38:31 +0900

    fix: test error

--- src/app/page/explorer/accounts/accounts.component.spec.ts
+++ src/app/page/explorer/accounts/accounts.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 import { AccountsComponent } from './accounts.component';
 
@@ -9,6 +11,20 @@ describe('AccountsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AccountsComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({
+              pageSize: 100,
+              pageNumber: 1,
+              order: 'desc',
+              orderBy: 'balance',
+              mosaicId: '310378C18A140D1B',
+            }),
+          },
+        },
+      ],
     }).compileComponents();
   });
 
