59ae8f7b6f583213afad3490f335744f09616975
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-09-23 05:39:20 +0900

    feat: add parent component of blocks component

---
+++ src/app/page/explorer/blocks/blocks.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blocks',
+  templateUrl: './blocks.component.html',
+  styleUrls: ['./blocks.component.css'],
+})
+export class BlocksComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    console.log('ngOnInit BlocksComponent');
+  }
+}
---
+++ src/app/page/explorer/blocks/blocks.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BlocksRoutingModule } from './blocks-routing.module';
+import { BlocksComponent } from './blocks.component';
+
+@NgModule({
+  declarations: [BlocksComponent],
+  imports: [CommonModule, BlocksRoutingModule],
+})
+export class BlocksModule {}
