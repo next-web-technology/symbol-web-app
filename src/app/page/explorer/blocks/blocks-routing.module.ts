d741998a11c7a2eda7e67cb2af58e770b09cb9f8
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-09-23 14:13:33 +0900

    feat: add parent block component

--- src/app/page/explorer/blocks/blocks-routing.module.ts
+++ src/app/page/explorer/blocks/blocks-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlockComponent } from './block/block.component';
 import { BlocksComponent } from './blocks.component';
 
 const routes: Routes = [
   {
     path: '',
     component: BlocksComponent,
+  },
+  {
+    path: ':height',
+    component: BlockComponent,
   },
 ];
 
--- src/app/page/explorer/blocks/blocks.module.ts
+++ src/app/page/explorer/blocks/blocks.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { BlocksRoutingModule } from './blocks-routing.module';
 import { BlocksComponent } from './blocks.component';
 import { ViewBlocksModule } from 'src/app/view/explorer/blocks/blocks.module';
+import { BlockComponent } from './block/block.component';
 
 @NgModule({
-  declarations: [BlocksComponent],
+  declarations: [BlocksComponent, BlockComponent],
   imports: [CommonModule, BlocksRoutingModule, ViewBlocksModule],
 })
 export class BlocksModule {}
