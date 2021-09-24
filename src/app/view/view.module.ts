810769fdc888d9f490409215e7efe7efbc8ddc57
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-09-25 06:56:57 +0900

    fix: unnecessary import

--- src/app/view/view.module.ts
+++ src/app/view/view.module.ts
@@ -1,13 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ViewHomeModule } from './home/home.module';
-import { ViewExplorerModule } from './explorer/explorer.module';
-import { ViewAccountsModule } from './explorer/accounts/accounts.module';
-import { ViewAccountModule } from './explorer/accounts/account/account.module';
-import { ViewBlocksModule } from './explorer/blocks/blocks.module';
-import { ViewBlockModule } from './explorer/blocks/block/block.module';
-import { ViewWalletModule } from './wallet/wallet.module';
-
 @NgModule({
   declarations: [],
   imports: [CommonModule],
